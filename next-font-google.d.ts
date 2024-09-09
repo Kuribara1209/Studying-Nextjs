7fea14404bc8ed8d1e99142f1d92a89b0e831e27
yukari_kuribara <yukari.kuribara@aillis.jp>
2024-09-09 14:14:49 +0900

    fix

--- next-font-google.d.ts
+++ next-font-google.d.ts
@@ -1,4 +1,4 @@
 declare module 'next/font/google' {
-  export function Inter(options: { subsets: string[] }): any;
-  export function Lusitana(options: { subsets: string[] }): any;
+  export function Inter(options: { subsets: string[], weight?: string[] }): any;
+  export function Lusitana(options: { subsets: string[], weight?: string[] }): any;
 }
